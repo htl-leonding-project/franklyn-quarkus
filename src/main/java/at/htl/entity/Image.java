d5a4340b81814393febb41b7b17181e839966e98
tamaraMelcher <t.melcher.htlleonding@gmail.com>
2022-01-25 14:14:25 +0100

    fra-9 created fields, constructor in image

--- src/main/java/at/htl/entity/Image.java
+++ src/main/java/at/htl/entity/Image.java
@@ -1,4 +1,26 @@
 package at.htl.entity;
 
-public class Image {
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "F_IMAGE")
+public class Image extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "I_ID", nullable = false)
+    private Long id;
+
+    @Column(name = "I_BYTES")
+    private byte[] imgInByte;
+
+    //region Constructor
+    public Image() {
+    }
+
+    public Image(byte[] imgInByte) {
+        this.imgInByte = imgInByte;
+    }
+    //endregion Constructor
 }
