f0f0088e9a47de1256b9c2847264804df79f2266
MichaelTran6624 <michael.tran6624@gmail.com>
2022-01-30 20:08:23 +0100

    fra-13 moved saved screenshots to directory

--- frontend/src/main/java/at/htl/control/InitBean.java
+++ frontend/src/main/java/at/htl/control/InitBean.java
@@ -10,9 +10,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Timer;
 
+
 public class InitBean {
     void init(@Observes StartupEvent event) {
         int count = 0;
+        File file = new File("./Test-Screenshots/");
+        file.mkdir();
         while (count < 10) {
             count++;
             try {
@@ -22,7 +25,7 @@ public class InitBean {
 
                 Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
                 BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
-                ImageIO.write(screenFullImage, format, new File(fileName));
+                ImageIO.write(screenFullImage, format, new File("./Test-Screenshots/" + fileName));
 
                 System.out.println("A full screenshot saved!");
                 Thread.sleep(5000);
