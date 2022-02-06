06db4cbf077d0c44ba017a26b02eab80be3a7a2d
MichaelTran6624 <michael.tran6624@gmail.com>
2022-02-06 13:23:48 +0100

    fra-10 modify InitBean

--- frontend/src/main/java/at/htl/control/InitBean.java
+++ frontend/src/main/java/at/htl/control/InitBean.java
@@ -1,7 +1,6 @@
 package at.htl.control;
 
-import at.htl.boundary.ImageResource;
-import at.htl.client.FileService;
+import at.htl.boundary.ImageService;
 import io.quarkus.runtime.StartupEvent;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
@@ -12,52 +11,38 @@ import javax.inject.Inject;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
+
+// https://stackoverflow.com/questions/66347707/send-a-simple-post-request-from-quarkus-java
 
 @ApplicationScoped
 public class InitBean {
-    String DIRECTORYNAME = "./test-screenshots/";
-
-    @Inject
-    ImageResource resource;
 
     @Inject
     @RestClient
-    FileService fileService;
+    ImageService imageService;
 
     void init(@Observes StartupEvent event) {
         int count = 0;
-        File file = new File(DIRECTORYNAME);
-        file.mkdir();
         while (count < 10) {
             count++;
             try {
                 Robot robot = new Robot();
                 String format = "jpg";
-                String fileName = "FullScreenshot"+count+"." + format;
+                String fileName = "fullscreenshot-"+count+"." + format;
 
                 Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
                 BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
-                File newFile = new File(DIRECTORYNAME+fileName);
+                File newFile = new File(fileName);
                 ImageIO.write(screenFullImage, format, newFile);
 
-                resource.sentImage(Files.newInputStream(newFile.toPath()));
+                System.out.println(imageService.uploadFile(newFile, fileName));
 
                 System.out.println("A full screenshot saved!");
                 Thread.sleep(5000);
             } catch (AWTException | IOException | InterruptedException ex) {
                 System.err.println(ex);
             }
-            // https://stackoverflow.com/questions/66347707/send-a-simple-post-request-from-quarkus-java
-            try {
-                fileService.postFile(new FileOutputStream(file), file.getName());
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-
         }
     }
 }
