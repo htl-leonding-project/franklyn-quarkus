b733293185027fce4d9c8a7e5fa002ba46e132bc
tamaraMelcher <t.melcher.htlleonding@gmail.com>
2022-03-26 16:42:13 +0100

    fra-35 created tests

---
+++ backend/src/test/java/at/htl/franklynserver/entity/ExamineeTest.java
@@ -0,0 +1,19 @@
+package at.htl.franklynserver.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExamineeTest {
+    @Test
+    void add_examinee_simple_success() {
+        Examinee examinee = new Examinee();
+        examinee.firstName = "Tamara";
+        examinee.lastName = "Melcher";
+
+        assertEquals("Tamara", examinee.firstName);
+        assertEquals("Melcher", examinee.lastName);
+    }
+
+
+}
