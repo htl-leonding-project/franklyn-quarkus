60ab5b73eade1c7509bf11cec6b248bd31aaec40
Tamara Melcher <t.melcher.htlleonding@gmail.com>
2022-11-17 23:03:52 +0100

    fra-90 add navbar

--- franklyn-frontend/src/app/app.module.ts
+++ franklyn-frontend/src/app/app.module.ts
@@ -33,6 +33,7 @@ import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
 import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
 import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
 import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,8 @@ import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
     NewTestComponent,
     SidebarComponent,
     HowtoComponent,
-    ScreenshotsComponent
+    ScreenshotsComponent,
+    NavbarComponent
     ],
   imports: [
     BrowserModule,
