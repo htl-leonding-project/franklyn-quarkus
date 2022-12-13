dc244f7bcf432c2611956c150cdda4089ff10d25
tamaraMelcher <t.melcher.htlleonding@gmail.com>
2022-12-13 15:31:08 +0100

    fra-81 change get to post

--- backend/src/main/java/at/htl/boundary/WebUntisResource.java
+++ backend/src/main/java/at/htl/boundary/WebUntisResource.java
@@ -3,10 +3,7 @@ package at.htl.boundary;
 import at.htl.service.WebUntisService;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("api/webuntis")
@@ -15,10 +12,10 @@ public class WebUntisResource {
     @Inject
     WebUntisService webUntisService;
 
-    @GET
-    @Path("authUser/{userName}/{password}")
+    @POST
+    @Path("authUser/{userName}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String authUser(@PathParam("userName") String userName, @PathParam("password") String password){
+    public String authUser(@PathParam("userName") String userName, String password){
         String response = webUntisService.authenticateUser(userName, password);
         return response;
     }
