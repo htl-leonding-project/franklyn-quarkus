bcf16628566f43cf94ec60badfd850e24de9770c
tamaraMelcher <t.melcher.htlleonding@gmail.com>
2023-01-25 14:10:59 +0100

    envs

--- franklyn-frontend/src/environments/environment.prod.ts
+++ franklyn-frontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'https://student.cloud.htl-leonding.ac.at/api'
 };
--- franklyn-frontend/src/environments/environment.ts
+++ franklyn-frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'https://student.cloud.htl-leonding.ac.at/api'
+  API_URL: 'http://localhost:8080/api'
 };
 
 /*
