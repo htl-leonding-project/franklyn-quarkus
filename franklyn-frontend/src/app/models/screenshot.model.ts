27f88f38138d45f3cdd35e11d618add17beb9aec
MichaelTran6624 <michael.tran6624@gmail.com>
2023-03-06 16:47:52 +0100

    fra-76 modify file

--- franklyn-frontend/src/app/models/screenshot.model.ts
+++ franklyn-frontend/src/app/models/screenshot.model.ts
@@ -1,8 +1,10 @@
 import { NgbPaginationNumber } from "@ng-bootstrap/ng-bootstrap"
+import {Byte} from "@angular/compiler/src/util";
 
 export interface Screenshot {
     image: string,
     screenshotId: number,
     examineeId: number,
-    examId: number
+    examId: number,
+    file: Byte[]
 }
