f46986946079f6206ccb0bde292e09f5e47618f7
MichaelTran6624 <michael.tran6624@gmail.com>
2023-03-06 17:00:31 +0100

    update main

--- franklyn-client/src/main/java/at/htl/boundary/ExamineeService.java
+++ franklyn-client/src/main/java/at/htl/boundary/ExamineeService.java
@@ -12,6 +12,7 @@ import javax.ws.rs.core.MediaType;
 @RegisterRestClient(configKey="client-api")
 public interface ExamineeService {
 
+
     /**
      * @param id        id of the exam
      * @param firstName firstname of the student
