5fc1aa3ddad55412749cc21cb91bf8d60be548f9
tamaraMelcher <t.melcher.htlleonding@gmail.com>
2023-04-13 20:19:47 +0200

    fra-120 add video service

---
+++ franklyn-frontend/src/app/services/video.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Forms} from "../models/forms.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VideoService {
+  private BASE_URL: string = environment.API_URL+ "/download";
+  constructor(private http: HttpClient) { }
+
+  downloadVideo(examineeId: string, examId: string){
+    return this.http.get<string>(this.BASE_URL + "/video/" + examId + "/" + examineeId);
+  }
+}
