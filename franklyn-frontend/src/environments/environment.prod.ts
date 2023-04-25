55bb52406d311a7b7ac8e7b68584902e865f9b43
tamaraMelcher <t.melcher.htlleonding@gmail.com>
2023-04-25 10:14:41 +0200

    update deployment

--- franklyn-frontend/src/environments/environment.prod.ts
+++ franklyn-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://student.cloud.htl-leonding.ac.at/t.stuetz/franklyn3/api'
+  API_URL: 'https://student.cloud.htl-leonding.ac.at/t.stuetz/franklyn/api'
 };
