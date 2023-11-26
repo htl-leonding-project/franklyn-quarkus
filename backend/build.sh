10e8f08add9e34593a879a992d2ac05bc3ba2472
Abdulrahman-AL-Sabagh <abdsabagh8@gmail.com>
2023-11-26 22:59:09 +0100

    feat(deployment) repair backend dockerfile and the build.sh in the ./backend (#99)

--- backend/build.sh
+++ backend/build.sh
@@ -5,5 +5,5 @@ set -e
 rm -rf target
 mvn -B clean package
 mkdir -p target/deploy
-cp target/*-runner.jar target/deploy/
+cp target/quarkus-app/quarkus-run.jar target/deploy/franklyn-application-server-image.jar
 docker build --tag franklyn-application-server-image --file ./src/main/docker/Dockerfile ./target/deploy
