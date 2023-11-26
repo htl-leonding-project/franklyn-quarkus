4acd5f64492654b316640ef65f1ec0fb8bc61ae0
P-Klose <ipad@klose.at>
2023-11-26 18:26:21 +0100

    fix(client): update to new /api URL

--- franklyn-client/src/main/java/at/htl/boundary/UserService.java
+++ franklyn-client/src/main/java/at/htl/boundary/UserService.java
@@ -8,7 +8,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/exam")
+@Path("/api/exam")
 @RegisterRestClient(configKey = "exam-api")
 public interface UserService {
 
