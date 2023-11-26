999b545442c6d19ffc0d3a96579211e08270e196
Abdulrahman-AL-Sabagh <abdsabagh8@gmail.com>
2023-11-26 23:14:43 +0100

    feat(deployment) repair borken deployment, so that the jar file could run on k8s (#99)

--- backend/build.sh
+++ backend/build.sh
@@ -5,5 +5,5 @@ set -e
 rm -rf target
 mvn -B clean package
 mkdir -p target/deploy
-cp target/quarkus-app/quarkus-run.jar target/deploy/franklyn-application-server-image.jar
+cp target/quarkus-app/quarkus-run.jar target/deploy/
 docker build --tag franklyn-application-server-image --file ./src/main/docker/Dockerfile ./target/deploy
