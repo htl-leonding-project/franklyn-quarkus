d344180898b470083390ae4ad69c509a473c4ae8
P-Klose <ipad@klose.at>
2024-01-10 11:06:17 +0100

    empty frame-backend projekt added

---
+++ frame-backend/src/test/java/at/htl/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package at.htl;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
